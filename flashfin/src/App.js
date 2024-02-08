07b289facfed4eb4560e77df6d5e9d709af1275a
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2024-02-08 18:22:01 -0500

    added files

---
+++ flashfin/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="expanded-sidebar">
+      {/* add custom logo */}
+      <h1>FlashFin</h1> 
+      <img id="user_pfp" src="" alt="User Pic"></img>
+      <button>Dashboard</button>
+      <button>Expenses</button>
+      <button>Settings</button>
+      <h2>My Accounts</h2>
+      <h3>PNC</h3>
+
+
+    </div>
+  );
+}
+
+export default App;
