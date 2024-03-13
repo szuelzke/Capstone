a12069a92da5900d0062e8a1f25f15506aac5e10
bgall1 <bgall1@kent.edu>
2024-03-13 13:38:30 -0400

    edits

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+@app.route('/home/')
+def home():
+    msg = ''
+    return render_template('index.html', msg=msg)
+
