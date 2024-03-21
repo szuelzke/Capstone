8f21e111338e281ff13540825be66758135ffc10
bgall1 <bgall1@kent.edu>
2024-03-21 17:32:13 -0400

    edit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
-@app.route('/home/')
+@app.route('/')
 def home():
     msg = ''
     return render_template('index.html', msg=msg)
