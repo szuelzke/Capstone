2b9294c618dfd9deb39a25ee3b90047c0268f6c0
bgall1 <bgall1@kent.edu>
2024-03-24 13:34:47 -0400

    edit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+import mysql.connector
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
+
+cnx = mysql.connector.connect(user='capstone', password='CapStone2024',
+                              host='localhost',
+                              database='FLASHFIN')
+cnx.close()
 
 @app.route('/')
 def home():
