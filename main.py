164562ef05cc0b8317afa9ddcf91b4753423a5a4
szuelzke <97550335+szuelzke@users.noreply.github.com>
2024-04-04 10:59:32 -0400

    test

--- main.py
+++ main.py
@@ -181,6 +181,7 @@ def settings():
         #phone = session['phone']
         #email = session['email']
         #created_date = session['current_date']
+        
         user_id = session['user_id']
         db_session = Session()
         user = db_session.query(User).filter_by(user_id=user_id).first()
@@ -188,7 +189,7 @@ def settings():
 
         #return render_template('settings.html')
         #return render_template('settings.html', first_name = first_name, last_name = last_name, phone = phone, email = email, created_date = created_date)
-        return render_template('settings.html', first_name = user.first_name, last_name = user.last_name, phone = user.phone, email = user.email, created_date = user.created_date)
+        return render_template('settings.html', user=user)
 
     else:
         return redirect(url_for('login'))
