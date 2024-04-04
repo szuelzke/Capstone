1131680e4376096acd80145b429593e9cbab2cc1
szuelzke <97550335+szuelzke@users.noreply.github.com>
2024-04-04 11:31:37 -0400

    test

--- main.py
+++ main.py
@@ -74,10 +74,10 @@ def login():
             session['user_id'] = user.user_id
             session['email'] = user.email
 
-            otp_secret = pyotp.random_base32()  # Generate new OTP secret for each login attempt
-            otp_uri = pyotp.totp.TOTP(otp_secret).provisioning_uri(user.email, issuer_name="FlashFin")
-            session['otp_secret'] = otp_secret  # Store OTP secret in session
-            session['otp_uri'] = otp_uri  # Store OTP URI in session
+            #otp_secret = pyotp.random_base32()  # Generate new OTP secret for each login attempt
+            #otp_uri = pyotp.totp.TOTP(otp_secret).provisioning_uri(user.email, issuer_name="FlashFin")
+            #session['otp_secret'] = otp_secret  # Store OTP secret in session
+            #session['otp_uri'] = otp_uri  # Store OTP URI in session
 
             return redirect(url_for('verify_2fa'))
 
@@ -100,7 +100,7 @@ def verify_2fa():
            session['user_id'] = get_user_id(session['email'])
            return redirect(url_for('home'))
         else:
-           return render_template('2fa.html', error='Invalid OTP', qr_code='', setup_key='')
+           return render_template('verify_2fa.html', error='Invalid OTP', qr_code='', setup_key='')
 
     otp_uri = session['otp_uri']
 
@@ -114,7 +114,7 @@ def verify_2fa():
     setup_key = pyotp.TOTP(session['otp_secret']).secret
 
     # Render the template with QR code and setup key
-    return render_template('2fa.html', otp_uri=otp_uri, qr_code=qr_base64, setup_key=setup_key)
+    return render_template('verify_2fa.html', otp_uri=otp_uri, qr_code=qr_base64, setup_key=setup_key)
 
 def get_user_id(email):
     db_session = Session()
