7d807324f9f6e716f1a16f6432faa73753f29957
bgall1 <bgall1@kent.edu>
2024-04-06 18:58:00 -0400

    edits

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def home():
         user_id = session['user_id']
         db_session = Session()
         user = db_session.query(User).filter_by(user_id=user_id).first()
-        accounts = db_session.query(Account).filter_by(user_id=user_id)
+        accounts = db_session.query(Account).filter_by(user_id=user_id).first()
         db_session.close()
         return render_template('index.html', user=user, accounts=accounts)
     else:
