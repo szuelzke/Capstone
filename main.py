7b559adbb3a12644527b0f21c62aa65c67cc6ea0
bgall1 <bgall1@kent.edu>
2024-04-06 21:50:28 -0400

    edits

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_USERNAME'] = 'flashfin.alerts@gmail.com'
+app.config['MAIL_PASSWORD'] = 'PSMGB2024!'
 
 mail = Mail(app)
 
