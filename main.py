c3332cfc2f9f58da7461fc7bb3b7d3d5e588436f
szuelzke <97550335+szuelzke@users.noreply.github.com>
2024-04-07 09:35:22 -0400

    test

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'flashfin.alerts@gmail.com'
-app.config['MAIL_PASSWORD'] = 'x'
+app.config['MAIL_PASSWORD'] = 'PSMGB2024!'
 
 mail = Mail(app)
 
