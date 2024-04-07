6210622b25a2a64fcf7fc24675f19facc8cf9102
szuelzke <97550335+szuelzke@users.noreply.github.com>
2024-04-07 18:38:06 -0400

    test

--- main.py
+++ main.py
@@ -117,6 +117,78 @@ except Exception as e:
     print("Connection failed:", e)
 
 
+#### ------------------------------- Utility Functions --------------------------------------------------------
+
+def get_user_id(email):
+    db_session = Session()
+    user = db_session.query(User).filter_by(email=email).first()
+    db_session.close()
+    return user.user_id if user else None
+
+def generate_reset_token():
+    return secrets.token_urlsafe(32)
+
+def calculate_expiry_time():
+    expiry_time = datetime.now() + time.timedelta(hours=1)
+    return expiry_time
+
+def send_reset_password_email(user_email, reset_token):
+    subject = "Reset Your Password"
+    sender = "flashfin.alerts@gmail.com"
+    recipients = [user_email]
+    text_body = f"Hello,\n\nPlease click the following link to reset your password:\n\nReset Password Link: http://capstone1.cs.kent.edu/reset-password/{reset_token}\n\nIf you did not request a password reset, please ignore this email.\n\nBest regards,\nYour FlashFin Team"
+    html_body = f"<p>Hello,</p><p>Please click the following link to reset your password:</p><p><a href='http://capstone1.cs.kent.edu/reset-password/{reset_token}'>Reset Password Link</a></p><p>If you did not request a password reset, please ignore this email.</p><p>Best regards,<br>Your FlashFin Team</p>"
+
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+
+    mail.send(msg)
+
+# Handling Settings
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Handling Accounts
+
+# returns dictionary of accounts connected to user
+def get_account_list():
+    user_id = session['user_id']
+    db_session = Session()
+    accounts = db_session.query(Account).filter_by(user_id=user_id).all()
+    account_list = {}
+    for account in accounts:
+        recent_transaction = db_session.query(Transaction).filter_by(account_id=account.account_id).order_by(Transaction.date.desc()).first()
+        account_list[account.account_id] = {}
+        account_list[account.account_id]["name"] = account.account_name
+        if recent_transaction:
+            account_list[account.account_id]["balance"] = recent_transaction.amount_remaining
+        else:
+            account_list[account.account_id]["balance"] = "0.00"
+    db_session.close()
+    return account_list
+
+# Alerts 
+'''
+# Function to send SMS
+def send_sms(to, body):
+    message = client.messages.create(
+        body=body,
+        from_=twilio_phone_number,
+        to=to
+    )
+    print("SMS sent to", to)
+
+# Function to check balance and send alert
+def check_balance_and_send_alert(user_phone, balance):
+    if balance < 50.00:
+        message = f"FlashFin: Your balance is ${balance:.2f}. "
+        send_sms(user_phone, message)
+'''
+
+
 #### ------------------------------- Handling Login System --------------------------------------------------------
 
 
@@ -126,7 +198,7 @@ def home():
         user_id = session['user_id']
         db_session = Session()
         user = db_session.query(User).filter_by(user_id=user_id).first()
-        account_list = utility.get_account_list()
+        account_list = get_account_list()
         db_session.close()
         return render_template('index.html', user=user, account_list=account_list)
     else:
@@ -257,15 +329,15 @@ def forgot_password():
 
         if user:
             # Generate and store reset token
-            reset_token = utility.generate_reset_token()
+            reset_token = generate_reset_token()
             user.reset_token = reset_token
-            user.reset_token_expiry = utility.calculate_expiry_time()
+            user.reset_token_expiry = calculate_expiry_time()
 
 
             db_session.commit()
             db_session.close()
             
-            utility.send_reset_password_email(email, reset_token)
+            send_reset_password_email(email, reset_token)
 
             #return render_template('password_reset_link_sent.html', email=email)
             return redirect(url_for('password_reset_link_sent', email=email))
@@ -333,7 +405,7 @@ def settings():
         user = db_session.query(User).filter_by(user_id=user_id).first()
         accounts = db_session.query(Account).filter_by(user_id=user_id).all()
         db_session.close()
-        account_list = utility.get_account_list()
+        account_list = get_account_list()
         
         end_time = time.time()  # Performance monitoring - end time
         logger.info(f"Settings request processed in {end_time - start_time} seconds")
@@ -532,7 +604,7 @@ def account(account_id):
 
         db_session.close()
 
-        return render_template('dashboard.html', account=account, transactions=transactions, user=user, balance=balance, account_list=utility.get_account_list())
+        return render_template('dashboard.html', account=account, transactions=transactions, user=user, balance=balance, account_list=get_account_list())
     else:
         return redirect(url_for('login'))
 
@@ -560,7 +632,7 @@ def transactions(account_id):
                 # get transactions 
             transactions_list = db_session.query(Transaction).filter_by(account_id=account_id).order_by(Transaction.date.desc()).all()
             db_session.close()
-            return render_template('transactions.html',transactions=transactions_list, user=user, account=account, account_list=utility.get_account_list())
+            return render_template('transactions.html',transactions=transactions_list, user=user, account=account, account_list=get_account_list())
         else:
             return redirect(url_for('login'))
     else:
@@ -654,7 +726,7 @@ def budget(account_id):
         db_session = Session()
         user = db_session.query(User).filter_by(user_id=user_id).first()
         account = db_session.query(Account).filter_by(user_id=user_id, account_id=account_id).first()
-        return render_template('budget.html', user=user, account=account, account_list=utility.get_account_list())
+        return render_template('budget.html', user=user, account=account, account_list=get_account_list())
 
 @app.route('/account/transaction/share', methods=['POST', 'GET'])
 def sharetransaction():
