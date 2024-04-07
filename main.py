9b5917134691d4e181b084d7e4e6aa2414a69ca6
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2024-04-07 03:15:10 -0400

    test

--- main.py
+++ main.py
@@ -99,10 +99,10 @@ def home():
         user = db_session.query(User).filter_by(user_id=user_id).first()
         accounts = db_session.query(Account).filter_by(user_id=user_id).all()
         # get amount remaining
-        balance_list = []
+        balance_list = {}
         for account in accounts:
             recent_transaction = db_session.query(Transaction).filter_by(account_id=account.account_id).order_by(Transaction.date.desc()).first()
-            balance_list.append(recent_transaction)
+            balance_list[account.account_name] = recent_transaction.amount_remaining
 
         db_session.close()
         return render_template('index.html', user=user, accounts=accounts, balance_list=balance_list)
