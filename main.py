375d32876116785f50649eebdfc084ae8e266166
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2024-04-07 04:00:54 -0400

    test

--- main.py
+++ main.py
@@ -92,20 +92,26 @@ except Exception as e:
 #### Handling Login System 
 
 @app.route('/', methods=['GET','POST'])
+def get_account_list():
+    user_id = session['user_id']
+    db_session = Session()
+    accounts = db_session.query(Account).filter_by(user_id=user_id).all()
+    # get user account id, name, and balance remaining
+    account_list = {}
+    for account in accounts:
+        recent_transaction = db_session.query(Transaction).filter_by(account_id=account.account_id).order_by(Transaction.date.desc()).first()
+        account_list[account.account_id] = {}
+        account_list[account.account_id]["name"] = account.account_name
+        account_list[account.account_id]["balance"] = recent_transaction.amount_remaining
+    db_session.close()
+    return account_list
+
 def home():
     if 'user_id' in session and session.get('mfa_completed', False):
         user_id = session['user_id']
         db_session = Session()
         user = db_session.query(User).filter_by(user_id=user_id).first()
-        accounts = db_session.query(Account).filter_by(user_id=user_id).all()
-        # get user account id, name, and balance remaining
-        account_list = {}
-        for account in accounts:
-            recent_transaction = db_session.query(Transaction).filter_by(account_id=account.account_id).order_by(Transaction.date.desc()).first()
-            account_list[account.account_id] = {}
-            account_list[account.account_id]["name"] = account.account_name
-            account_list[account.account_id]["balance"] = recent_transaction.amount_remaining
-
+        account_list = get_account_list()
         db_session.close()
         return render_template('index.html', user=user, account_list=account_list)
     else:
