ceffea701c0c1c2bebf143ae9d67a113a942e808
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2024-04-14 15:46:56 -0400

    test

--- main.py
+++ main.py
@@ -1065,9 +1065,9 @@ def sharetransaction(account_id, transaction_id):
             if active_share:
                 other_social = ""
                 if active_share.sender_id == user_id:
-                    other_social = active_share.receiver.social_name
-                else:
                     other_social = active_share.sender.social_name
+                else:
+                    other_social = active_share.receiver.social_name
                 db_session.close()
                 return render_template('share_transaction.html', user=user, account=account, transaction=transaction, active_share=active_share, other_social=other_social, msg='This transaction is already shared with another user')
             else:
