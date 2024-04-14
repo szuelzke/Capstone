5a1c944b95637fa41367febfa6bfbf69280a43df
melissa-underwood <100211401+melissa-underwood@users.noreply.github.com>
2024-04-14 02:01:13 -0400

    test

--- main.py
+++ main.py
@@ -924,7 +924,7 @@ def filter_transactions(account_id):
         categories = db_session.query(Budget).filter_by(account_id=account_id).all()
         if account:
             # get transactions 
-            transactions_list = db_session.query(Transaction).filter(Transaction.date.between(start_date, end_date)).all()
+            transactions_list = db_session.query(Transaction).filter(Transaction.date == start_date).all()
             return render_template('transactions.html',transactions=transactions_list, user=user, account=account, categories=categories)
         else:
             return redirect(url_for('add_account'))
